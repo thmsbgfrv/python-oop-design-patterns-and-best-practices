de8b2f1909e33af320d18f3392fb785fc2339210
Tahnasib Gafarov <tahnasibgafarov@gmail.com>
2024-10-01 11:24:17 +0200

    changed instance from class variable to private varibale

--- oop/patterns/creational/singleton.py
+++ oop/patterns/creational/singleton.py
@@ -11,7 +11,7 @@ class Singleton:
     """
     # pylint: disable=too-few-public-methods
 
-    _instance: Optional["Singleton"] = None  # Store the singleton instance
+    __instance: Optional["Singleton"] = None  # Store the singleton instance
     config: Dict[str, str] = {"db": "test"}  # Example configuration dictionary
 
     def __new__(cls) -> "Singleton":
@@ -19,9 +19,9 @@ class Singleton:
         This method returns the instance if it has been created before.
         If not, it creates a new instance.
         """
-        if cls._instance is None:
-            cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
+        if cls.__instance is None:
+            cls.__instance = super(Singleton, cls).__new__(cls)
+        return cls.__instance
 
     @classmethod
     def get_instance(cls) -> "Singleton":
